5eb46445cb8e824a9c08c422ddc7c63877dd8414
Sapna2001 <harisapnanair@gmail.com>
2021-08-22 02:01:56 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const messageButton = document.getElementById("sendMessageButton");
 const phone = document.getElementById("phone");
 
+function phoneNumberValid(phoneNumber) {
+    if(phoneNumber.match(/^\d{10}$/)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 function sendMessage() {
     let phoneNumber = phone.value;
     // console.log(phoneNumber)   
-    let url = `https://api.whatsapp.com/send?phone=91${phoneNumber}&text=`
-    location.href = url;
-    console.log(url);
+    if(phoneNumberValid(phoneNumber)) {
+        let url = `https://api.whatsapp.com/send?phone=91${phoneNumber}&text=`
+        location.href = url;    
+    } else {
+        alert("Please enter a valid number");
+    }
+    
 }
 
 messageButton.addEventListener("click", sendMessage);
