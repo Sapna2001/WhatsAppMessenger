a226e63fe94d9febd7bc67fbdd6ce481b76cf1b7
Sapna2001 <harisapnanair@gmail.com>
2021-08-25 19:19:29 +0530

    bug fixed

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function phoneNumberValid(phoneNumber) {
 }
 
 function sendMessage() {
-    let phoneNumber = phone.value.trim();
+    let phoneNumber = phone.value.split(" ").join("");
     // console.log(phoneNumber)   
     if(phoneNumberValid(phoneNumber)) {
         let url = `https://api.whatsapp.com/send?phone=91${phoneNumber}&text=`
